16d5943b6fddf365f08770e68fbb3e4c3dc67eac
Helcio <helcioanicio19@gmail.com>
2023-06-08 21:58:14 -0300

    Update Js to request dates of API

--- src/script.js
+++ src/script.js
@@ -3,28 +3,37 @@
 // const publicApiKey = 
 // const privateApiKey = 
 
-const search = document.querySelector('#search')
-const searchButton = document.querySelector('.fa-search')
+const authentication = 'apikey=8e30179e891ae0ca9d101872b0217c7e&hash=232cc1a3d5ad13b80cf2ce6d224bb2f4&ts=1'
+const apikey = '8e30179e891ae0ca9d101872b0217c7e4a6594ff9575100686737c454f288a3d2e9f883d';
+
+const search = document.querySelector('#search');
+const searchButton = document.querySelector('.fa-search');
+
+const image = document.querySelector('.image');
+const characterName = document.querySelector('h4');
+const Paragraph = document.querySelector('p');
+
+
+
 
 const getAPI = async (searchValue) => {
-    const apiURL = `https://gateway.marvel.com:443/v1/public/characters?${publicApiKey}`
-    const res = await fetch(apiURL);
-    const data = await res.json();
+    const apiURL = `http://gateway.marvel.com/v1/public/characters?${searchValue}&${authentication}`
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    console.log(data)
     return data;
   };
   
-  const showRequest = async (searchValue) => {
-    const data = await getAPI(searchValue);
-    console.log(data);
-  }
-// console.log()
-
-
+//   const showRequest = (searchValue) => {
+//     const data = getAPI (searchValue);
+//     console.log(data);
+//   }
+// getAPI()
 
 //  EVENT
 searchButton.addEventListener('click', ()=>{
     const searchValue = search.value;
-    showRequest(searchValue)
+    getAPI(searchValue)
 })
 
 
