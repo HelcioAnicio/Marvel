a812f60f01fcd23d782ec055380a7ae7e23e2b39
Helcio <helcioanicio19@gmail.com>
2023-06-10 22:25:31 -0300

    Change some logic in JS

--- src/script.js
+++ src/script.js
@@ -1,7 +1,3 @@
-// const apiKey = process.env.API_KEY;
-// const hash = process.env.HASH;
-// const authentication = `apikey=${apiKey}&hash=${hash}&ts=1`
-
 const authentication = `apikey=8e30179e891ae0ca9d101872b0217c7e&hash=232cc1a3d5ad13b80cf2ce6d224bb2f4&ts=1`
 
 const search = document.querySelector('#search');
@@ -24,23 +20,22 @@ const getAPI = async(searchValue) => {
   
   const showRequest = async(searchValue) => {
     const data = await getAPI(searchValue);
+    search.value = ''
 
     characterName.innerHTML = data.data.results[0].name;
     paragraph.innerHTML = data.data.results[0].description;
     image.setAttribute(
     "src",
     `${data.data.results[0].thumbnail.path}/detail.jpg`
-    // `http://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0`
-    // `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
   );
-  
-
-
   }
+
+
 
 //  EVENT
 searchButton.addEventListener('click', ()=>{
     const searchValue = search.value;
+    console.log(searchValue)
     showRequest(searchValue);
 })
 
