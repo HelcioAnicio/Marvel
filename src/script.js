017257316b5c4063e16213b6c64b0c17841e7774
Helcio <helcioanicio19@gmail.com>
2023-06-15 23:16:00 -0300

    Tried to create new div to add random images on my home

--- src/script.js
+++ src/script.js
@@ -7,46 +7,78 @@ const image = document.querySelector('.image');
 const characterName = document.querySelector('.nameCharacterSearched');
 const paragraph = document.querySelector('.descriptionSearched');
 
+const divCaracter = document.querySelector('.divCaracter');
+
 // Request Random
 const getApiAll = async() => {
-    const maxCharacters = 1500;
-    const offset = Math.floor((Math.random() * maxCharacters) + 1);
+  const maxCharacters = 1500;
+  const offset = Math.floor((Math.random() * maxCharacters) + 1);
 
-    const apiUrlAll = `https://gateway.marvel.com/v1/public/characters?limit=9&offset=${offset}&${authentication}`
-    const response = await fetch(apiUrlAll);
-    const data = await response.json();
-    console.log(data)
-    return data;
-  };
+  const apiUrlAll = `https://gateway.marvel.com/v1/public/characters?limit=9&offset=${offset}&${authentication}`
+  const response = await fetch(apiUrlAll);
+  const data = await response.json();
+  return data;
+};
 
+async function useApi() {
+  const data = await getApiAll();
+  data.data.results.forEach(element => {
+    // divCaracter.appendChild()
+    createBox()
+    
 
+  });
+}
 
+// useApi()
+createBox = () => {
+  let divCaracter = document.createElement('div')
+  divCaracter.className = 'divCarater'
+  let divImage = document.createElement('div')
+  divImage.className = 'divImage'
+  let image = document.createElement('img')
+  image.className = 'imageApi'
+  let divText = document.createElement('div')
+  divText.className = 'divText'
+  let nameCharacter = document.createElement('h4')
+  nameCharacter.className = 'nameCharacter'
+  let paragraph = document.createElement('p')
+  paragraph.className = 'paragraph'
+
+  divCaracter.appendChild(divImage);
+  divImage.appendChild(image);
+  divCaracter.appendChild(divText);
+  divText.appendChild(nameCharacter);
+  divText.appendChild(paragraph);
+
+  const sectionTwo = document.querySelector('.sectionTwo');
+  sectionTwo.appendChild(divCaracter)
+}
+
+createBox()
 
 
 
 // Request after click
 const getApiOne = async(searchValue) => {
-    const apiUrlOne = `https://gateway.marvel.com/v1/public/characters?name=${searchValue}&${authentication}`
-    const response = await fetch(apiUrlOne);
-    const data = await response.json();
-    console.log(data)
-    return data;
-  };
+  const apiUrlOne = `https://gateway.marvel.com/v1/public/characters?name=${searchValue}&${authentication}`
+  const response = await fetch(apiUrlOne);
+  const data = await response.json();
+  console.log(data)
+  return data;
+};
 
-  
-  const showRequest = async(searchValue) => {
-    const data = await getApiOne(searchValue);
-    search.value = ''
+const showRequest = async(searchValue) => {
+  const data = await getApiOne(searchValue);
+  search.value = ''
 
-    characterName.innerHTML = data.data.results[0].name;
-    paragraph.innerHTML = data.data.results[0].description;
-    image.setAttribute(
-    "src",
-    `${data.data.results[0].thumbnail.path}/detail.jpg`
-  );
-  }
-
-
+  characterName.innerHTML = data.data.results[0].name;
+  data.data.results[0].description == '' ? paragraph.innerHTML = 'No description at the moment' : paragraph.innerHTML = data.data.results[0].description;
+  image.setAttribute(
+  "src",
+  `${data.data.results[0].thumbnail.path}/detail.jpg`
+);
+}
 
 //  EVENT
 searchButton.addEventListener('click', ()=>{
