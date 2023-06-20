18fdc3bc7e7ca039c809be88157a4dea95e4e29e
Helcio <helcioanicio19@gmail.com>
2023-06-19 21:39:35 -0300

    Created the section on project about characters random

--- src/script.js
+++ src/script.js
@@ -3,80 +3,83 @@ const authentication = `apikey=8e30179e891ae0ca9d101872b0217c7e&hash=232cc1a3d5a
 const search = document.querySelector('#search');
 const searchButton = document.querySelector('.fa-search');
 
-const image = document.querySelector('.image');
-const characterName = document.querySelector('.nameCharacterSearched');
-const paragraph = document.querySelector('.descriptionSearched');
+const imageSearched = document.querySelector('.imageSearched');
+const characterNameSearched = document.querySelector('.nameCharacterSearched');
+const paragraphSearched = document.querySelector('.descriptionSearched');
 
-const divCaracter = document.querySelector('.divCaracter');
 
 // Request Random
-const getApiAll = async() => {
+const getApiRandom = async() => {
   const maxCharacters = 1500;
   const offset = Math.floor((Math.random() * maxCharacters) + 1);
 
-  const apiUrlAll = `https://gateway.marvel.com/v1/public/characters?limit=9&offset=${offset}&${authentication}`
-  const response = await fetch(apiUrlAll);
+  const apiUrlSearched = `https://gateway.marvel.com/v1/public/characters?limit=9&offset=${offset}&${authentication}`
+  const response = await fetch(apiUrlSearched);
   const data = await response.json();
   return data;
 };
 
-async function useApi() {
-  const data = await getApiAll();
-  data.data.results[0].forEach(element => {
+async function useApi(divCharacterRandom, divImageRandom, imageRandom, divTextRandom, nameCharacterRandom, paragraphRandom) {
+  const data = await getApiRandom();
+  data.data.results.forEach(element => {
     console.log(element)
-    createBox()
-    image.setAttribute(
-      "src",
-      `${element.thumbnail.path}/detail.${element.thumbnail.extension}`
-    ) 
-    nameCharacter.innerHTML = `${element.name}` 
+
+    createBox(element)
+    // imageRandom.setAttribute(
+    //   "src",
+    //   `${element.thumbnail.path}/detail.jpg`
+    // ) 
+    // nameCharacterRandom.innerHTML = `${element.name}` 
   });
 }
 
 // useApi()
-createBox = () => {
-  let divCaracter = document.createElement('div')
-  divCaracter.className = 'divCarater'
-  let divImage = document.createElement('div')
-  divImage.className = 'divImage'
-  let image = document.createElement('img')
-  image.className = 'imageApi'
-  let divText = document.createElement('div')
-  divText.className = 'divText'
-  let nameCharacter = document.createElement('h4')
-  nameCharacter.className = 'nameCharacter'
-  let paragraph = document.createElement('p')
-  paragraph.className = 'paragraph'
-
-  divCaracter.appendChild(divImage);
-  divImage.appendChild(image);
-  divCaracter.appendChild(divText);
-  divText.appendChild(nameCharacter);
-  divText.appendChild(paragraph);
+createBox = (element) => {
+  
+  console.log('dentro do create box', element)
+  let divCharacterRandom = document.createElement('div')
+  divCharacterRandom.className = 'divCharacterRandom'
+  divImageRandom = document.createElement('div')
+  divImageRandom.className = 'divImageRandom'
+  imageRandom = document.createElement('img')
+  imageRandom.className = 'imageRandom'
+  imageRandom.src = 'assets/Marvel_universe.png'
+  divTextRandom = document.createElement('div')
+  divTextRandom.className = 'divTextRandom'
+  nameCharacterRandom = document.createElement('h4')
+  nameCharacterRandom.className = 'nameCharacterRandom'
+  nameCharacterRandom.innerText = element.name
+  paragraphRandom = document.createElement('p')
+  paragraphRandom.className = 'paragraphRandom'
+  
+  divCharacterRandom.appendChild(divImageRandom);
+  divImageRandom.appendChild(imageRandom);
+  divCharacterRandom.appendChild(divTextRandom);
+  divTextRandom.appendChild(nameCharacterRandom);
+  divTextRandom.appendChild(paragraphRandom);
 
   let sectionTwo = document.querySelector('.sectionTwo');
-  sectionTwo.appendChild(divCaracter)
+  sectionTwo.appendChild(divCharacterRandom)
 }
 useApi()
 
 
-
 // Request after click
-const getApiOne = async(searchValue) => {
-  const apiUrlOne = `https://gateway.marvel.com/v1/public/characters?name=${searchValue}&${authentication}`
-  const response = await fetch(apiUrlOne);
+const getApiOneSearched = async(searchValue) => {
+  const apiUrlSearched = `https://gateway.marvel.com/v1/public/characters?name=${searchValue}&${authentication}`
+  const response = await fetch(apiUrlSearched);
   const data = await response.json();
   console.log(data)
   return data;
 };
 
-const showRequest = async(searchValue) => {
-  const data = await getApiOne(searchValue);
+const showRequestSearched = async(searchValue) => {
+  const data = await getApiOneSearched(searchValue);
   search.value = ''
 
-  characterName.innerHTML = data.data.results[0].name;
-  data.data.results[0].description == '' ? paragraph.innerHTML = 'No description at the moment' : paragraph.innerHTML = data.data.results[0].description;
-  image.setAttribute(
+  characterNameSearched.innerHTML = data.data.results[0].name;
+  data.data.results[0].description == '' ? paragraphSearched.innerHTML = 'No description at the moment' : paragraphSearched.innerHTML = data.data.results[0].description;
+  imageSearched.setAttribute(
   "src",
   `${data.data.results[0].thumbnail.path}/detail.jpg`
 );
@@ -88,7 +91,7 @@ const showRequest = async(searchValue) => {
 searchButton.addEventListener('click', ()=>{
     const searchValue = search.value;
     getApiAll()
-    showRequest(searchValue);
+    showRequestSearched(searchValue);
 })
 
 
